816cf40539ae3297f9decbef4195a059e97ca5ba
CGC <1099899538@qq.com>
2018-06-30 16:38:59 +0800

    出去不必要using

--- work_study_entire/u_student/Apply.aspx.cs
+++ work_study_entire/u_student/Apply.aspx.cs
@@ -1,22 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Configuration;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using System.Data;
-using System.Data.SqlClient;
-
-using System.Drawing;
-using System.IO;
-using System.Text;
-using System.Data.OleDb;
-using System.Reflection;
-using System.Threading;
 using System.Text.RegularExpressions;
 
 
